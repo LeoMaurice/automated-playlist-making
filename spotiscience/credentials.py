bdb1c15a66d13bed5f4455ae3a235268b9431e09
Léopold Maurice <leopold.maurice@m4x.org>
2022-12-29 22:14:43 +0100

    Update credentials.py

--- spotiscience/credentials.py
+++ spotiscience/credentials.py
@@ -2,9 +2,9 @@
 
 CREDENTIALS = {
 
-    'client_id': "673d947303b242c5a4908ce5a9cdc2bd",
-    'client_secret': "5464973400484f838c4f11d467144b1d",
-    'redirect_url': "http://localhost:8080/oauth/return",
-    'user_id' : "draskeglom",
-    'genius_access_token':"WDrAJIUUrjxIO8OG5RYIAE0mFnjX5Vn751roNGyxHMUWLeqAj_CEUeFZKkSfNgzf"
+    'client_id': "",
+    'client_secret': "",
+    'redirect_url': "",
+    'user_id' : "",
+    'genius_access_token':""
     }
